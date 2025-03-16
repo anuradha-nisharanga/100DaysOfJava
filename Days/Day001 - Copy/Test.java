ebef8e141f043e1ffefe1864e7beb91ffa6221be
anuradha <anuradhanisharanga09@gmail.com>
2025-03-16 23:48:36 +0530

    changed folder structure

---
+++ Days/Day001 - Copy/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        int [] numberList = {1, 2, 3, 4, 5};
+
+        int sum = 0;
+
+        for (int i = 0; i < numberList.length; i++) {
+            sum = sum + numberList[i];
+        }
+
+        System.out.println(sum);
+     
+    }
+}
