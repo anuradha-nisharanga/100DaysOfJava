041087870411d53c6500bff03c6f80884ffb0ed1
anuradha <anuradhanisharanga09@gmail.com>
2025-03-19 23:50:03 +0530

    solved Reverse String question

---
+++ Days/Day004/Task.java
@@ -0,0 +1,21 @@
+package Day004;
+
+// Reverse a String
+public class Task {
+
+    public static void main(String[] args) {
+        
+        String value  = "Hellow!";
+        String reversedValue = "";
+
+        for (int i = 0; i < value.length(); i++) {
+            
+            reversedValue = value.charAt(i) + reversedValue;
+            System.out.println(reversedValue);
+        }
+
+
+
+    }
+    
+}
