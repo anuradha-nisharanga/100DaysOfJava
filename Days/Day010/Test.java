c996008683fd48398afb767bce35ec3c0d41ddc9
anuradha <anuradhanisharanga09@gmail.com>
2025-03-25 23:56:11 +0530

    Learned Do While and While Loop

---
+++ Days/Day010/Test.java
@@ -0,0 +1,22 @@
+package Day010;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        int i=5;
+
+        while (i<10) {
+            i++;
+            System.out.println("Anuradha");
+        }
+
+        System.out.println("=======================");
+        System.out.println(i); // i = 10
+        
+        do{
+            i++;
+            System.out.println("Anuradha");
+        }while(i<15);
+    }
+}
