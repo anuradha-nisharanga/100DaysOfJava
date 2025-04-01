99e7504592c54a82f45bd69710a7f15ca5bc3086
anuradha <anuradhanisharanga09@gmail.com>
2025-04-01 23:56:30 +0530

    Solid principles - Single responsibility

---
+++ Days/Day015/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        // SOLID principles
+        // S - Single responsibility
+        // O - Open/Closed
+        // L - Liskov Substitution
+        // I - Interface Segregation
+        // D - Depenendency Inversion
+
+        // Single Responsibility  - a class should only have a one responsibility
+        
+        User user = new User("Anuradha", "anuradha@gmail.com");
+        UserFileManager fileManager = new UserFileManager();
+        fileManager.saveToFile(user);
+    }
+}
---
+++ Days/Day015/User.java
@@ -0,0 +1,21 @@
+
+// User class only handle the user related data and fucntionalities
+
+public class User {
+    
+    private String name;
+    private String email;
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+}
